856efa4bd21d99a55158e270878bdd784c135eeb
brianedwardherrera@gmail.com <brianedwardherrera@gmail.com>
2025-02-11 02:27:10 +0000

    Created pair_feed; Deleted Form2

--- client_code/pair_feed/__init__.py
+++ client_code/pair_feed/__init__.py
@@ -1,8 +1,8 @@
-from ._anvil_designer import Form2Template
+from ._anvil_designer import pair_feedTemplate
 from anvil import *
 
 
-class Form2(Form2Template):
+class pair_feed(pair_feedTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
